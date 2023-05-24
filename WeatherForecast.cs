c07582c96611e5523e4d7a4d7c3a0c7929037553
mgbrodi <mbrodi@vmware.com>
2023-05-24 16:40:07 -0400

    :)

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,4 +9,9 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Summary} - Date: {Date} - C: {TemperatureC} - F: {TemperatureF}";
+    }
 }
